374cc685659250d6d92a8462e4b89d4e06be8290
Akshay Raina <akshayraina999@gmail.com>
2026-06-01 15:18:38 +0530

    [FIX] removed standard docker build commands and used kaniko approach

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,5 +1,18 @@
-// jenkins-shared-library/vars/buildImage.groovy
-def call(Map config) {
-    echo "🏗️ Building Docker Image: ${config.imageName}:${config.tag}"
-    sh "docker build -t ${config.imageName}:${config.tag} ."
+// vars/buildImage.groovy
+def call(Map config = [:]) {
+    def imageName = config.required('imageName')
+    def tag       = config.required('tag')
+    def context   = config.get('context', '.')
+    def dockerfile = config.get('dockerfile', 'Dockerfile')
+
+    echo "🏗️  Orchestrating Cloud-Native Kaniko Build Layer..."
+    
+    // Kaniko uses standard build flags instead of subcommands
+    sh """
+        /kaniko/executor \
+          --context=${context} \
+          --dockerfile=${dockerfile} \
+          --destination=${imageName}:${tag} \
+          --skip-tls-verify
+    """
 }
