9e9805002c89af833b54f63367c3601035dfe0e7
Akshay Raina <akshayraina999@gmail.com>
2026-06-01 15:08:13 +0530

    [FIX] sonarqube name change

--- vars/sonarScan.groovy
+++ vars/sonarScan.groovy
@@ -1,8 +1,17 @@
-// jenkins-shared-library/vars/sonarScan.groovy
-def call(Map config) {
-    echo "🔍 Triggering SonarQube Static Code Analysis..."
-    // Expects a SonarQube environment setup in Jenkins Global Configurations
-    withSonarQubeEnv('LocalSonarQube') {
-        sh "./gradlew sonar" // Assuming Gradle/Java build engine
+// vars/sonarScan.groovy
+def call(Map config = [:]) {
+    def stageName = config.get('stageName', 'Static Code Analysis')
+    
+    stage(stageName) {
+        // Utilizing the official Jenkins SonarQube plugin environment block
+        withSonarQubeEnv('SonarQube-Local') {
+            echo "🚀 Initiating SonarQube Quality Scanner Analysis..."
+            
+            // Execute the Maven sonar goal against our local cluster engine
+            sh "./mvnw clean verify sonar:sonar \
+                -Dsonar.projectKey=${config.projectKey} \
+                -Dsonar.projectName=${config.projectName} \
+                -Dsonar.host.url=http://sonarqube-sonarqube.devops-tools.svc.cluster.local:9000"
+        }
     }
 }
